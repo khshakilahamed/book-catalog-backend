5e698f51daa1c5670c1075fdcfc8c3a1783cf38a
khshakilahamed <khshakil.ahamed18@gmail.com>
2023-09-04 17:05:13 +0600

    Live link added with readme file

--- dist/shared/logger.js
+++ dist/shared/logger.js
@@ -1,48 +1,54 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorLogger = exports.logger = void 0;
-const winston_1 = require("winston");
-const { combine, timestamp, label, printf } = winston_1.format;
-const path_1 = __importDefault(require("path"));
-const winston_daily_rotate_file_1 = __importDefault(require("winston-daily-rotate-file"));
-// Custom log format
-const myFormat = printf(({ level, message, label, timestamp }) => {
-    const date = new Date(timestamp);
-    const hour = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    return `${date.toDateString()} ${hour}:${minutes}:${seconds} [${label}] ${level}: ${message}`;
-});
-const logger = (0, winston_1.createLogger)({
-    level: 'info',
-    format: combine(label({ label: 'BookCatalog' }), timestamp(), myFormat),
-    transports: [
-        new winston_1.transports.Console(),
-        new winston_daily_rotate_file_1.default({
-            filename: path_1.default.join(process.cwd(), 'logs', 'winston', 'successes', 'phu-%DATE%-success.log'),
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '14d',
-        }),
-    ],
-});
-exports.logger = logger;
-const errorLogger = (0, winston_1.createLogger)({
-    level: 'error',
-    format: combine(label({ label: 'BookCatalog' }), timestamp(), myFormat),
-    transports: [
-        new winston_1.transports.Console(),
-        new winston_daily_rotate_file_1.default({
-            filename: path_1.default.join(process.cwd(), 'logs', 'winston', 'errors', 'phu-%DATE%-error.log'),
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '14d',
-        }),
-    ],
-});
-exports.errorLogger = errorLogger;
+// import { createLogger, format, transports } from 'winston';
+// const { combine, timestamp, label, printf } = format;
+// import path from 'path';
+// import DailyRotateFile from 'winston-daily-rotate-file';
+// // Custom log format
+// const myFormat = printf(({ level, message, label, timestamp }) => {
+//   const date = new Date(timestamp);
+//   const hour = date.getHours();
+//   const minutes = date.getMinutes();
+//   const seconds = date.getSeconds();
+//   return `${date.toDateString()} ${hour}:${minutes}:${seconds} [${label}] ${level}: ${message}`;
+// });
+// const logger = createLogger({
+//   level: 'info',
+//   format: combine(label({ label: 'BookCatalog' }), timestamp(), myFormat),
+//   transports: [
+//     new transports.Console(),
+//     new DailyRotateFile({
+//       filename: path.join(
+//         process.cwd(),
+//         'logs',
+//         'winston',
+//         'successes',
+//         'phu-%DATE%-success.log',
+//       ),
+//       datePattern: 'YYYY-MM-DD-HH',
+//       zippedArchive: true,
+//       maxSize: '20m',
+//       maxFiles: '14d',
+//     }),
+//   ],
+// });
+// const errorLogger = createLogger({
+//   level: 'error',
+//   format: combine(label({ label: 'BookCatalog' }), timestamp(), myFormat),
+//   transports: [
+//     new transports.Console(),
+//     new DailyRotateFile({
+//       filename: path.join(
+//         process.cwd(),
+//         'logs',
+//         'winston',
+//         'errors',
+//         'phu-%DATE%-error.log',
+//       ),
+//       datePattern: 'YYYY-MM-DD-HH',
+//       zippedArchive: true,
+//       maxSize: '20m',
+//       maxFiles: '14d',
+//     }),
+//   ],
+// });
+// export { logger, errorLogger };
